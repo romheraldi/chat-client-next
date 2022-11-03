7b245504a6524a4af85a1b2764b11505b8449cb2
Romualdus Aldi Hermanto <romheraldi.dev@gmail.com>
2022-11-03 17:34:04 +0700

    Update: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import styles from '../styles/Home.module.css'
-import {io, Socket} from "socket.io-client";
-import {useEffect} from "react";
+import ButtonLink from "../components/button/buttonLink";
 
 export default function Home() {
-  let socket: Socket
-
-  useEffect(() => {
-    socket = io('http://localhost:3000')
-
-    socket.on('connect', () => {
-      console.log("Connected to server")
-    })
-  })
   return (
-    <div className={styles.container}>
-
-    </div>
+      <div className="flex flex-col content-center justify-items-center items-center center mt-32">
+          <h1 className="text-3xl mb-10 font-bold underline">
+              Welcome to Nestian!
+          </h1>
+          <p className={"w-96 text-center"}>
+              Nestian is chat application built in NestJS as a server and NextJS as a client side. With this application your are be able to chat with another user whose registered to the app.
+          </p>
+          <h2 className={"mt-5 text-2xl"}>Let's invite them to the chat!</h2>
+          <div className="mt-10">
+              <ButtonLink text={"Register"} link={"/register"} className={""}/>
+              <ButtonLink text={"Login"} link={"/login"} className={"mx-2"}/>
+          </div>
+      </div>
   )
 }
