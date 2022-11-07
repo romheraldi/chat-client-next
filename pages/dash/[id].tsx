52ddcf2d1ef31e3122490ba23156b9fd5454b670
Romualdus Aldi Hermanto <romheraldi.dev@gmail.com>
2022-11-07 15:57:28 +0700

    update: feature message done

---
+++ pages/dash/[id].tsx
@@ -0,0 +1,127 @@
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import axios from "axios";
+import {deleteCookie, getCookie} from "cookies-next";
+import {io} from "socket.io-client";
+import Link from "next/link";
+
+type Room = {
+    id: number
+    socket: string
+    user: User
+    another_user: User
+}
+
+type User = {
+    id: number
+    username: string
+}
+
+type Message = {
+    id: number
+    message: string
+    sender: User
+    room: Room
+}
+
+export default function Dash() {
+    const router = useRouter()
+    const {id} = router.query
+    const [user, setUser] = useState<User>({} as User)
+    const [room, setRoom] = useState<Room>({} as Room)
+    const [messages, setMessages] = useState<Message[]>([])
+    const [newMessage, setNewMessage] = useState("")
+    let socket = io('http://localhost:3000')
+
+    useEffect(() => {
+        console.log("Hai")
+
+        socket.on('connect', () => {
+            console.log("Connected to socket server")
+        })
+
+        const nekot = getCookie('nekot')
+        if (!nekot) {
+            router.push('/login')
+        }
+
+        const userData = async () => {
+            try {
+                const result = await axios.get('http://localhost:3000/users/me', {
+                    headers: {
+                        'x-auth': getCookie('nekot')
+                    }
+                })
+
+                await setUser(result.data.data)
+            } catch (e: any) {
+                console.error(e.response.data)
+            }
+        }
+
+        userData()
+
+        const data = async () => {
+            console.log(`http://localhost:3000/rooms/${id}`)
+            try {
+                const result = await axios.get(`http://localhost:3000/rooms/${id}`, {
+                    headers: {
+                        'x-auth': getCookie('nekot')
+                    }
+                })
+
+                await setRoom(result.data.data)
+            } catch (e: any) {
+                console.error(e.response.data)
+            }
+        }
+
+        data()
+
+        socket.emit("fetchRoomMessages", room.socket)
+
+        socket.on('roomMessages', msg => setMessages(msg))
+
+        socket.on('message', (msg) => {
+            setMessages(oldMessages => [...oldMessages, msg])
+        })
+    }, [])
+
+    const sendMessage = async () => {
+        if (newMessage) {
+            console.log("Sending")
+            socket.emit('createMessage', {
+                room_id: room.id,
+                message: newMessage,
+                sender_id: user.id
+            })
+
+            setNewMessage("")
+        }
+    }
+
+    return (
+        <div className="flex flex-col content-center justify-items-center items-center center mt-32">
+            <h1 className="text-3xl mb-10 font-bold underline">
+                Let&apos;s chat with Nestian
+            </h1>
+            <div className="mt-5">
+                {
+                    messages.length > 0 ? messages.map((message) => {
+                        return <div><span className={"text-blue-500 font-bold"}>({message.sender.username})</span> {message.message}</div>
+                    }) : "No message here"
+                }
+            </div>
+            <div className="mt-32">
+                <input
+                    type="text"
+                    name="password"
+                    className={"text-center p-3"}
+                    placeholder="Your secret"
+                    value={newMessage}
+                    onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+                    onChange={(e) => setNewMessage(e.target.value)}/>
+            </div>
+        </div>
+    )
+}
--- pages/dash/index.tsx
+++ pages/dash/index.tsx
@@ -1,55 +1,110 @@
-import {useEffect, useState} from "react";
-import {NextResponse} from "next/server";
-import ButtonLink from "../../components/button/buttonLink";
+import React, {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import axios from "axios";
 import {deleteCookie, getCookie} from "cookies-next";
+import {io} from "socket.io-client";
+import Link from "next/link";
+
+type Room = {
+    id: number
+    socket: string
+    user: User
+    another_user: User
+}
+
+type User = {
+    id: number
+    username: string
+}
 
 export default function Dash() {
-	const router = useRouter()
-	const [user, setUser] = useState({username: ''})
-	useEffect( () => {
-		const nekot = getCookie('nekot')
-		if (!nekot) {
-			router.push('/login')
-		}
+    const router = useRouter()
+    const [user, setUser] = useState({id: null, username: ''})
+    const [rooms, setRooms] = useState<Room[]>([])
+    let socket = io('http://localhost:3000')
 
-		checkUser()
-	})
+    useEffect(() => {
 
-	const checkUser = async () => {
-		try {
-			const response = await axios.get('http://localhost:3000/users/me', {
-				headers: {
-					'x-auth': getCookie('nekot')
-				}
-			})
+        socket.on('connect', () => {
+            console.log("Connected to socket server")
+        })
 
-			console.log(response.data)
-			setUser(response.data.data)
-		} catch(e: any) {
-			console.log(e.response?.data.message)
-		}
-	}
+        const nekot = getCookie('nekot')
+        if (!nekot) {
+            router.push('/login')
+        }
 
-	const logout = () => {
-		deleteCookie('nekot')
-		router.push('/login')
-	}
-	return (
-		<div className="flex flex-col content-center justify-items-center items-center center mt-32">
-			<h1 className="text-3xl mb-10 font-bold underline">
-				Welcome back to Nestian, {user.username}!
-			</h1>
-			<p className={"w-96 text-center"}>
-				Please select one of usernames below or add your friend!
-			</p>
-			<div>
+        const data = async () => {
+            try {
+                const result = await axios.get('http://localhost:3000/users/me', {
+                    headers: {
+                        'x-auth': getCookie('nekot')
+                    }
+                })
 
-			</div>
-			<div className="mt-10">
-				<button onClick={() => logout()} className={"px-5 py-2 border-2 hover:bg-white hover:text-black"}>Logout</button>
-			</div>
-		</div>
-	)
+                await setUser(result.data.data)
+            } catch (e: any) {
+                console.error(e.response.data)
+            }
+        }
+
+        data()
+    }, [])
+
+    useEffect(() => {
+        const getRooms = async () => {
+            try {
+                console.log("Getting room")
+                const result = await axios.get('http://localhost:3000/users/my-rooms', {
+                    headers: {
+                        'x-auth': getCookie('nekot')
+                    }
+                })
+                await setRooms(result.data.data)
+            } catch (e: any) {
+                console.error(e.response.data)
+            }
+        }
+
+        getRooms()
+        if (user.id) {
+            socket.emit('join', `member-${user.id}`)
+            socket.emit('fetchRooms', {id: user.id})
+        }
+    }, [])
+
+    const logout = () => {
+        deleteCookie('nekot')
+        socket.disconnect()
+        router.push('/login')
+    }
+    return (
+        <div className="flex flex-col content-center justify-items-center items-center center mt-32">
+            <h1 className="text-3xl mb-10 font-bold underline">
+                Welcome back to Nestian, {user.username}!
+            </h1>
+            <p className={"w-96 text-center mb-5"}>
+                Please select one of usernames below or add your friend!
+            </p>
+            <div className="mt-5">
+                {
+                    rooms.length > 0 ? rooms.map((room) => {
+                        console.log(room.user?.id === user.id)
+                        if (room.user?.id === user.id) {
+                            return <Link href={`/dash/${room.socket}`}
+                                         className="mt-5 border-2 p-3 hover:bg-white hover:text-black">{room.another_user?.username}</Link>
+                        } else {
+                            return <Link href={`/dash/${room.socket}`}
+                                className="mt-5 border-2 p-3 hover:bg-white hover:text-black">{room.user?.username}</Link>
+                        }
+                    }) : "No data here"
+                }
+            </div>
+            <div className="mt-10">
+                <button onClick={() => logout()}
+                        className={"px-5 py-2 border-2 hover:bg-white hover:text-black"}>Logout
+                </button>
+            </div>
+        </div>
+    )
 }
